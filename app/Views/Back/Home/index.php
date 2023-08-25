3cc963af8a8fa966944122bb8f65543fee8ebb1f
marciopelegrini <marcio.pelegrini@gmail.com>
2023-08-25 16:20:17 -0400

    Add Admin template

---
+++ app/Views/Back/Home/index.php
@@ -0,0 +1,29 @@
+<?= $this->extend('Back/Layout/main'); ?>
+
+
+<?= $this->section('title'); ?>
+<?= $title ?? 'Home'; ?>
+<?= $this->endSection(); ?>
+
+
+<?= $this->section('css'); ?>
+<?= $this->endSection(); ?>
+
+
+<?= $this->section('content'); ?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title ?? 'Home do super Admin'; ?></h1>
+    <?= $user; ?>
+
+</div>
+<!-- /.container-fluid -->
+
+<?= $this->endSection(); ?>
+
+
+<?= $this->section('js'); ?>
+<?= $this->endSection(); ?>
